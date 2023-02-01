14203e902445485cdcb7072caa014e74f96adc61
MiltonLiquinchana <liquinchana@hotmail.es>
2023-01-31 22:22:24 -0500

    se agrega la capacidad de generar clientes multiples

--- WebSocketServer/src/main/java/com/mflq/websocketserver/configuration/WebSocketConfig.java
+++ WebSocketServer/src/main/java/com/mflq/websocketserver/configuration/WebSocketConfig.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -28,4 +29,5 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
          * a un usario especifico*/
         registry.setUserDestinationPrefix("/user");
     }
+
 }
