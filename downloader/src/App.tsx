6fdf631f70cfeca87746f6df61ba69350d2e3c4f
MiltonLiquinchana <liquinchana@hotmail.es>
2023-02-04 00:00:30 -0500

    se crea un cliente con react que muestra una barra de progreso

---
+++ downloader/src/App.tsx
@@ -0,0 +1,12 @@
+import DownloaderComponent from './components/DownloaderComponent'
+
+function App() {
+
+  return (
+    <div >
+      <DownloaderComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ downloader/src/components/DownloaderComponent.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+import "../css/Downloader.css";
+import StompClientServiceImpl from "../implement/StompClientServiceImpl";
+import StompClientService from "../service/StompClientService";
+interface stylos {
+  width: string | number;
+}
+export default function DownloaderComponent() {
+  let stompClient: StompClientService;
+  /**Creamos una referencia hacia el progres */
+  const [downloadState, setDownloadState] = useState<string | number>('0%');
+
+  /**Con useEffect vacio solo creamos una vez la instancia del cliente */
+  useEffect(() => {
+    stompClient = new StompClientServiceImpl(setDownloadState);
+  }, []);
+  /**Creamos una instancia del cliente */
+
+  const styles: stylos = { width: downloadState };
+
+  return (
+    <div className="downloader">
+      <div className="progress">
+        <div
+          className="progress-bar progress-bar-striped progress-bar-animated"
+          role="progressbar"
+          aria-label="Animated striped example"
+          style={styles}
+          aria-valuenow={Number(downloadState.toString().replace('%',''))}
+          aria-valuemin={0}
+          aria-valuemax={100}
+        >
+          {downloadState}
+        </div>
+      </div>
+      <button
+        type="button"
+        className="btn btn-success"
+        onClick={() => {
+          stompClient.connect("stompWebClient");
+        }}
+      >
+        StartDownloader
+      </button>
+    </div>
+  );
+}
