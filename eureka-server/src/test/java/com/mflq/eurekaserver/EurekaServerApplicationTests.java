97fd7b287dd4d2c59184b0201d61f0e45ba137f6
MiltonLiquinchana <liquinchana@hotmail.es>
2023-03-31 11:31:36 -0500

    se agrega nuevo servicio eureka-server

---
+++ eureka-server/src/test/java/com/mflq/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mflq.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
