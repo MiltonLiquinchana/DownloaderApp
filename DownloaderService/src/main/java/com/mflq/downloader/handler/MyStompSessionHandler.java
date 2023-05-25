2a617c0e38980d85851d4f9f243c24a9d0718a64
MiltonLiquinchana <liquinchana@hotmail.es>
2023-05-24 20:45:57 -0500

    se agrega un metodo para la desconexion del cliente al finalizar la descarga, esto ayuda a liberar recursos

--- DownloaderService/src/main/java/com/mflq/downloader/handler/MyStompSessionHandler.java
+++ DownloaderService/src/main/java/com/mflq/downloader/handler/MyStompSessionHandler.java
@@ -1,60 +1,62 @@
 package com.mflq.downloader.handler;
 
-import java.lang.reflect.Type;
-
+import com.mflq.downloader.dto.DownloadResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
-import com.mflq.downloader.dto.DownloadResponse;
-
-import lombok.extern.log4j.Log4j2;
+import java.lang.reflect.Type;
 
 @Log4j2
 public class MyStompSessionHandler extends StompSessionHandlerAdapter {
-	private StompSession session;
-	private String downloadClientName;
+    private StompSession session;
+    private String downloadClientName;
 
-	public MyStompSessionHandler(String downloadClientName) {
-		this.downloadClientName = downloadClientName;
-	}
+    public MyStompSessionHandler(String downloadClientName) {
+        this.downloadClientName = downloadClientName;
+    }
 
-	/* Después de conectar */
-	@Override
-	public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
-		log.debug("Subscribiendo con el usuario {} afterConnected", this.downloadClientName);
-		// Nos subscribimos a un endpoint para escuchar los mensajes
-		session.subscribe("/user/" + this.downloadClientName + "/queue/notification", this);
-		this.session = session;
+    /* Después de conectar */
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+        log.debug("Subscribiendo con el usuario {} afterConnected", this.downloadClientName);
+        // Nos subscribimos a un endpoint para escuchar los mensajes
+        session.subscribe("/user/" + this.downloadClientName + "/queue/notification", this);
+        this.session = session;
 
-	}
+    }
 
-	/* En caso de que suceda un error */
-	@Override
-	public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload,
-			Throwable exception) {
-		log.error("Got an exception", exception);
-	}
+    /* En caso de que suceda un error */
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload,
+                                Throwable exception) {
+        log.error("Got an exception", exception);
+    }
 
-	/* Este metodo establece el tipo de mensaje saliente */
-	@Override
-	public Type getPayloadType(StompHeaders headers) {
-		log.debug("Estableciendo typos de mensajes salientes getPayloadType");
+    /* Este metodo establece el tipo de mensaje saliente */
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        log.debug("Estableciendo typos de mensajes salientes getPayloadType");
 
-		/* Por el momento el mensaje es de tipo DownloadResponse.class */
-		return DownloadResponse.class;
-	}
+        /* Por el momento el mensaje es de tipo DownloadResponse.class */
+        return DownloadResponse.class;
+    }
 
-	/* Este metodo maneja los mensajes recividos */
-	@Override
-	public void handleFrame(StompHeaders headers, Object payload) {
-		DownloadResponse msg = (DownloadResponse) payload;
-		log.debug("Mensaje recivido: {}", msg);
-	}
+    /* Este metodo maneja los mensajes recividos */
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        DownloadResponse msg = (DownloadResponse) payload;
+        log.debug("Mensaje recivido: {}", msg);
+    }
 
-	public void sendMessage(DownloadResponse downloadResponse) {
-		log.debug("Notificando al cliente");
-		this.session.send("/app/message", downloadResponse);
-	}
+    public void sendMessage(DownloadResponse downloadResponse) {
+        log.debug("Notificando al cliente");
+        this.session.send("/app/message", downloadResponse);
+    }
+
+    public void disconnectClient() {
+        this.session.disconnect();
+    }
 }
--- DownloaderService/src/main/java/com/mflq/downloader/implement/DownloaderServiceImpl.java
+++ DownloaderService/src/main/java/com/mflq/downloader/implement/DownloaderServiceImpl.java
@@ -1,21 +1,5 @@
 package com.mflq.downloader.implement;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URLConnection;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.file.Path;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
-import org.springframework.stereotype.Service;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-
 import com.mflq.downloader.dto.DownloadContructorRequest;
 import com.mflq.downloader.dto.DownloadContructorResponse;
 import com.mflq.downloader.dto.DownloadRequest;
@@ -26,95 +10,112 @@ import com.mflq.downloader.repository.DownloadFileRepository;
 import com.mflq.downloader.service.DownloaderService;
 import com.mflq.downloader.service.ProgressCallBackService;
 import com.mflq.downloader.util.RBCWrapper;
-
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.Path;
 
 @Log4j2
 @Service
 public class DownloaderServiceImpl implements DownloaderService {
 
-	/* Creamos una referencia al servicio de notificacion de progreso de descarga */
-	@Autowired
-	private ProgressCallBackService progressCallBackService;
+    /* Creamos una referencia al servicio de notificacion de progreso de descarga */
+    @Autowired
+    private ProgressCallBackService progressCallBackService;
 
-	/* Creamos una referencia hacia el repositorio de datos */
-	@Autowired
-	private DownloadFileRepository downloadFileRepository;
+    /* Creamos una referencia hacia el repositorio de datos */
+    @Autowired
+    private DownloadFileRepository downloadFileRepository;
 
-	/* metodo para crear clientes stomp, para cada descarga */
-	private MyStompSessionHandler createMyStompSessionHandler(DownloadRequest downloadRequest) {
-		log.info("Creando cliente Stomp createMyStompSessionHandler");
+    /* metodo para crear clientes stomp, para cada descarga */
+    private MyStompSessionHandler createMyStompSessionHandler(DownloadRequest downloadRequest) {
+        log.info("Creando cliente Stomp createMyStompSessionHandler");
 
-		/* Creamos un nuevo cliente WebSocketClient */
-		WebSocketClient client = new StandardWebSocketClient();
+        /* Creamos un nuevo cliente WebSocketClient */
+        WebSocketClient client = new StandardWebSocketClient();
 
-		/* Crea un nuevo cliente stomp con el cliente WebSocket */
-		WebSocketStompClient stompClient = new WebSocketStompClient(client);
+        /* Crea un nuevo cliente stomp con el cliente WebSocket */
+        WebSocketStompClient stompClient = new WebSocketStompClient(client);
 
-		/*
-		 * Agregamos un conversor de mensajes en este caso para manejar mensajes en
-		 * formato json
-		 */
-		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        /*
+         * Agregamos un conversor de mensajes en este caso para manejar mensajes en
+         * formato json
+         */
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
-		/* URL del servidor websocket */
-		String url = "ws://localhost:8080/ws";
+        /* URL del servidor websocket */
+        String url = "ws://localhost:8080/ws";
 
-		/* Creamos un nuevo manejador de sesiones con el nombre del archivo */
-		MyStompSessionHandler myStompSessionHandler = new MyStompSessionHandler(downloadRequest.getFileName());
+        /* Creamos un nuevo manejador de sesiones con el nombre del archivo */
+        MyStompSessionHandler myStompSessionHandler = new MyStompSessionHandler(downloadRequest.getFileName());
 
-		/* Creamos un nuevo manejador de sessiones stomp con el handler anterior */
-		StompSessionHandler sessionHandler = myStompSessionHandler;
+        /* Creamos un nuevo manejador de sessiones stomp con el handler anterior */
+        StompSessionHandler sessionHandler = myStompSessionHandler;
 
-		/*
-		 * Conectamos al servidor, pasamos la url y controlardor de la session(Este es
-		 * el que controla la manera de como manipular los mensajes que entran y salen)
-		 */
-		stompClient.connectAsync(url, sessionHandler);
-		/* Retornamos la intancia del cliente stomp */
-		return myStompSessionHandler;
+        /*
+         * Conectamos al servidor, pasamos la url y controlardor de la session(Este es
+         * el que controla la manera de como manipular los mensajes que entran y salen)
+         */
+        stompClient.connectAsync(url, sessionHandler);
+        /* Retornamos la intancia del cliente stomp */
+        return myStompSessionHandler;
 
-	}
+    }
 
-	@Override
-	public DownloadContructorResponse downloadContructor(DownloadContructorRequest downloadContructorRequest) {
-		DownloadFileProjection obj = downloadFileRepository.getDownloadFileById(1);
-		System.out.println(obj);
+    @Override
+    public DownloadContructorResponse downloadContructor(DownloadContructorRequest downloadContructorRequest) {
+        DownloadFileProjection obj = downloadFileRepository.getDownloadFileById(1);
+        System.out.println(obj);
 //		DownloadFileProjection downloadFileProjection = downloadFileRepository.getDownloadFileById(1);
-		/* Creamos un Objeto */
-		return new DownloadContructorResponse();
-	}
+        /* Creamos un Objeto */
+        return new DownloadContructorResponse();
+    }
 
-	@Override
-	public void downLoadFile(Path path, URLConnection urlConnection, long localFileSize,
-			DownloadRequest downloadRequest) {
-		log.info("Download initialize, {}", path.getFileName());
+    @Override
+    public void downLoadFile(Path path, URLConnection urlConnection, long localFileSize,
+                             DownloadRequest downloadRequest) {
+        log.info("Download initialize, {}", path.getFileName());
 
-		MyStompSessionHandler myStompSessionHandler = createMyStompSessionHandler(downloadRequest);
-		try (ReadableByteChannel rbc = new RBCWrapper(Channels.newChannel(urlConnection.getInputStream()),
-				localFileSize, (urlConnection.getContentLength() + localFileSize), this, myStompSessionHandler,
-				downloadRequest);
-				FileOutputStream fos = new FileOutputStream(path.toString(), true);
-				FileChannel fileChannel = fos.getChannel()) {
-			fileChannel.transferFrom(rbc, 0, Long.MAX_VALUE);
+        MyStompSessionHandler myStompSessionHandler = createMyStompSessionHandler(downloadRequest);
+        try (ReadableByteChannel rbc = new RBCWrapper(Channels.newChannel(urlConnection.getInputStream()),
+                localFileSize, (urlConnection.getContentLength() + localFileSize), this, myStompSessionHandler,
+                downloadRequest);
+             FileOutputStream fos = new FileOutputStream(path.toString(), true);
+             FileChannel fileChannel = fos.getChannel()) {
+            fileChannel.transferFrom(rbc, 0, Long.MAX_VALUE);
 
-		} catch (IOException e) {
-			log.error("Uh oh: " + e);
-		}
-	}
+        } catch (IOException e) {
+            log.error("Uh oh: " + e);
+        } finally {
+            myStompSessionHandler.disconnectClient();
+            log.info("Cliente {} desconectado", path.getFileName());
+        }
+    }
 
-	@Override
-	public void notifyDownloadProgres(long sizeRead, double progress, MyStompSessionHandler mysSessionHandler,
-			DownloadRequest downloadRequest) {
-		/* Este muestra el progreso de descarga en este hilo */
-		this.progressCallBackService.rbcProgressCallback(sizeRead, progress);
-		/*
-		 * Envía un mensaje al servidor socket para notificar al cliente del progreso de
-		 * descarga
-		 */
-		mysSessionHandler.sendMessage(
-				new DownloadResponse(downloadRequest.getFileName(), downloadRequest.getClient(), sizeRead, progress));
+    @Override
+    public void notifyDownloadProgres(long sizeRead, double progress, MyStompSessionHandler mysSessionHandler,
+                                      DownloadRequest downloadRequest) {
+        /* Este muestra el progreso de descarga en este hilo */
+        this.progressCallBackService.rbcProgressCallback(sizeRead, progress);
+        /*
+         * Envía un mensaje al servidor socket para notificar al cliente del progreso de
+         * descarga
+         */
+        mysSessionHandler.sendMessage(
+                new DownloadResponse(downloadRequest.getFileName(), downloadRequest.getClient(), sizeRead, progress));
 
-	}
+    }
 
 }
